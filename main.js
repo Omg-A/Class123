9bba498a3976411e84ad611ed7f498e9e9f197e4
Omg-A <67595078+Omg-A@users.noreply.github.com>
2021-09-13 11:53:54 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -10,7 +16,12 @@ function setup(){
 }
 
 function draw(){
+    document.getElementById("square_sides").innerHTML = "Width and Height of the square will be = " + difference;
+
     background('grey');
+    fill('pink');
+    stroke('black');
+    square(noseX, noseY, difference);
 }
 
 function  modelLoaded(){
@@ -20,5 +31,13 @@ function  modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X = " + noseX + "Nose Y = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("Left Wrist X = " + leftWristX + "Right Wrist X" + rightWristX + "Difference = " + difference);
     }
 }
